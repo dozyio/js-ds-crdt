0772d41d699a988548070ae599bc5b014f121b6c
dozyio <github@dozy.io>
2024-08-28 11:31:35 +0100

    feat: v0.0.20, attempted export fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-export { CRDTDatastore, defaultOptions } from './crdt'
-export { PubSubBroadcaster } from './pubsub_broadcaster'
-export { Heads } from './heads'
-export { CRDTNodeGetter } from './ipld'
-export { CRDTSet } from './set'
-export { msgIdFnStrictNoSign } from './utils'
+import { CRDTDatastore, defaultOptions } from './crdt'
+import { Heads } from './heads'
+import { CRDTNodeGetter } from './ipld'
+import { PubSubBroadcaster } from './pubsub_broadcaster'
+import { CRDTSet } from './set'
+import { msgIdFnStrictNoSign } from './utils'
+import type { CRDTLibp2pServices, Options } from './crdt'
 
-export type { CRDTLibp2pServices, Options } from './crdt'
+export { CRDTDatastore, defaultOptions }
+export { PubSubBroadcaster }
+export { Heads }
+export { CRDTNodeGetter }
+export { CRDTSet }
+export { msgIdFnStrictNoSign }
+export type { CRDTLibp2pServices, Options }
